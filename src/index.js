96e9599c510b41c257813e851174e72cef1b00a7
Nikita Kozyrev <nikk7100@mail.ru>
2024-09-24 23:40:22 +0300

    create game field func

--- src/index.js
+++ src/index.js
@@ -1,29 +1,53 @@
-const gnomeImgPath = require('./img/goblin.png');
+const gnomeImgPath = require('./img/goblin.png'); 
 import './style.css';
 
+function createGameField() {
+    const body = document.body;
+    const gameField = document.createElement('div');
+    gameField.className = 'game-field';
 
-function getRandomPosition() {
-    const rows = 4;
-    const cols = 4;
-    const position = {
-        top: Math.floor(Math.random() * rows) * 100,
-        left: Math.floor(Math.random() * cols) * 100,
-    };
-    return position;
+    const gnome = document.createElement('img');
+    gnome.id = 'gnome';
+    gnome.alt = 'Gnome';
+    gnome.src = "src/img/goblin.png"
+    gnome.style.position = 'absolute'; 
+    gnome.style.display = 'none'; 
+    gameField.appendChild(gnome);
+
+    for (let i = 0; i < 16; i++) { 
+        const cell = document.createElement('div');
+        cell.className = 'cell'; 
+        gameField.appendChild(cell);
+    }
+
+    body.appendChild(gameField);
 }
 
-function moveGnome() {
+createGameField(); 
+
+function getRandomPosition() { 
+    const rows = 4; 
+    const cols = 4; 
+    const position = { 
+        top: Math.floor(Math.random() * rows) * 100, 
+        left: Math.floor(Math.random() * cols) * 100, 
+    }; 
+    return position; 
+}
+
+function moveGnome() { 
     const gnome = document.getElementById('gnome');
 
     if (!gnome.src) {
-        gnome.src = gnomeImgPath;
+        gnome.src = gnomeImgPath; 
     }
 
     const { top, left } = getRandomPosition();
 
     gnome.style.top = `${top}px`;
     gnome.style.left = `${left}px`;
-    gnome.style.display = 'block';
+    gnome.style.display = 'block'; 
 }
 
-setInterval(moveGnome, 1000); 
+setInterval(moveGnome, 1000);
+
